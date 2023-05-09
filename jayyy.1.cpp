992c49ada329aba024e89e4e0ea9aaf8fedad916
Jaybhadiyadra22 <131762319+Jaybhadiyadra22@users.noreply.github.com>
2023-05-09 03:22:58 -0400

    Add files via upload

---
+++ jayyy.1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+ main()
+{
+	int x=2,y=8,xy;
+	xy = (x*x)-(2*x*y)+(y*y);
+	printf("x+y=%d",xy);
+	}
---
+++ jayyy3.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+ main()
+{
+	int x=2,y=8,xy;
+	xy = (x*x*x) + ((3*x*y)*(x+y)) + (y*y*y);
+
+	printf("x+y=%d",xy);
+	}
